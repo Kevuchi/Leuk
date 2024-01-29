3fe989c7e3d3e29c1835ba0c7a3243a14a1f894b
Kevin <aradnotikevin@gmail.com>
2024-01-29 09:41:07 -0300

    Product card rendering function, templates implemented but still not working properly

--- index.js
+++ index.js
@@ -2,211 +2,96 @@ let even = document.createElement('div')
 let currentPage = 1
 let odd = document.createElement('div')
 const FILEPATH = '/catalogo1.csv'
-//const file1 = fetch('https://docs.google.com/spreadsheets/d/1lJ5sEQ4Z95ydjyVKADxxvSflQOI32LX89ryOkZzXu-E/edit#gid=1552755221').then(file => file.text())
-// (async function getFile() {
-//     return await fetch('.\proeba.csv')
-// })
-//internet way
-
-// const file1 = Papa.parse('./proeba.csv', {
-//     header: true,
-//     download: true,
-//     dynamicTyping: true,
-//     complete: function (results) {
-//         // console.log('RESULTS', results.data);
-//         data = results.data;
-//     }
-// });
-//console.log(data)
-//washi's way
 let catalogueData = []
+
+
+// Example usage within your loop
+// for (let i = 1; i < data.length; i++) {
+//     if (!data[i][0] || data[i][0] == '' || data[i][0] == ' ') continue;
+
+//     if (i % 5 == 0 || i == 1) {
+//         if (currentPage % 2 !== 0) {
+//             const oddTemplate = document.querySelector('#odd');
+//             const oddTemplateClone = oddTemplate.content.cloneNode(true);
+
+//             // Now, select the product list within the cloned odd template
+//             const productList = oddTemplateClone.querySelector('.product-list');
+
+//             // Call the function to append product card
+//             appendProductCard(productList, data[i]);
+
+//             document.body.appendChild(oddTemplateClone);
+//         }
+//     }
+// }
+function appendProductCard(productList, productData) {
+    // console.log(productData)
+    const productCardTemplate = document.querySelector('#productTemplate');
+    const productCard = productCardTemplate.content.cloneNode(true);
+    productCard.querySelector('.title').innerHTML = `<br> ${productData[0]}`;
+    const detailsList = productCard.querySelector('.caracteristicas');
+    const imagePath = productData[1];
+    const imageDiv = productCard.querySelector('.image');
+    if (productData[14] === 'SI') {
+        const ribbon = document.createElement('div');
+        ribbon.classList.add('ribbon', 'left');
+        ribbon.textContent = 'nuevo';
+        imageDiv.appendChild(ribbon);
+    }
+    const image = productCard.querySelector('img');
+    image.src = imagePath
+    let detailCount = 0;
+    for (let i = 2; i < productData.length; i++) {
+        if (detailCount === 6) break;
+        if (productData[i] === '' || !productData[i] || productData[i] === ' ' || productData[i] === '-') continue;
+        const detail = document.createElement('li');
+        detail.innerText = productData[i];
+        detailsList.appendChild(detail);
+        detailCount++;
+    }
+    productList.appendChild(productCard);
+}
 async function readCatalogue() {
     const resp = await fetch(FILEPATH)
     const text = await resp.text()
     const rows = await Papa.parse(text)
     const data = rows.data
-    console.log('data!!', data)
-
-    //return data
-
-    const list = document.querySelector('.product-list')
+    // console.log('data!!', data)
 
     for (let i = 1; i < data.length; i++) {
 
         if (!data[i][0] || data[i][0] == '' || data[i][0] == ' ') continue
 
-        // if (i % 5 == 0) {
-        //     currentPage++;
-        //     const template = document.querySelector('.even')
+        if (i % 5 == 0 || i == 1) {
+            // currentPage++; sumar al final así pongo 
 
+            if (currentPage % 2 !== 0) {
+                // const oddTemplate = document.querySelector('#odd')
+                // const oddTemplateClone = oddTemplate.content.cloneNode(true)
+                // document.body.appendChild(oddTemplateClone)
+                const evenTemplate = document.querySelector('.even').content.cloneNode(true);
+                const list = evenTemplate.querySelector('.product-list');
+                const footerPageNum = evenTemplate.querySelector('.pageNum')
+                footerPageNum.innerHTML = currentPage;
+                console.log(data[i])
+                appendProductCard(list, data[i])
+                document.body.appendChild(evenTemplate)
 
-        // }
+            } else {
+                const tryTemplate = document.querySelector('#templatePrueba').content.cloneNode(true);
+                const list = tryTemplate.querySelector('.product-list');
+                const footerPageNum = tryTemplate.querySelector('.pageNum')
+                footerPageNum.innerHTML = currentPage;
+                console.log(data[i])
+                appendProductCard(list, data[i])
+                document.body.appendChild(tryTemplate)
+            }
 
-        const product = document.createElement('div')
-        const productTitle = data[i][0];
-        const productPicPath = data[i][1] ? data[i][1] : '';
-        // const productCategory = data[i][2] ? data[i][2] : '';
-        // const productMaterial = data[i][3] ? data[i][3] : '';
-        // const productLightType = data[i][4] ? data[i][4] : '';
-        // const productPotency = data[i][5] ? data[i][5] : '';
-        // const productTemperature = data[i][6] ? data[i][6] : '';
-        // const productColor = data[i][7] ? data[i][7] : '';
-        // const productHeight = data[i][8] ? data[i][8] : '';
-        // const productWidth = data[i][9] ? data[i][9] : '';
-        // const productLength = data[i][10] ? data[i][10] : '';
-        // const productDepth = data[i][11] ? data[i][11] : '';
-        // const productDiameter = data[i][12] ? data[i][12] : '';
-        // const productAditional = data[i][13] ? data[i][13] : '';
-        // const productNational = data[i][14];
-        // const productPrice = data[i][15];
-        const divImage = document.createElement('div')
-        divImage.className = 'image'
-        divImage.innerHTML = `  <img src=${productPicPath} alt="">`;
-        if (data[i][14] === 'SI') {
-            divImage.innerHTML =
-                `<div class="ribbon left">nuevo</div>
-                   <img src=${productPicPath} alt="">`;
-        }
-
-        const info = document.createElement('div');
-        info.className = 'info';
-        const details = document.createElement('div');
-        details.className = 'details';
-        const title = document.createElement('div');
-        title.className = 'title';
-        title.innerHTML = `<br> ${productTitle}`;
-        const detailsList = document.createElement('ul');
-        detailsList.className = 'caracteristicas';
-        let detailCount = 0
-        for (let j = 2; j < data[i].length; j++) {
-            if (detailCount == 6) break;
-            if (!data[i][j] || data[i][j] === '' || data[i][j] === ' ' || data[i][j] === '-') continue;
-            const detail = document.createElement('li')
-            detail.innerHTML = data[i][j]
-            detailsList.appendChild(detail)
-            detailCount++;
 
         }
-        details.append(title)
-        details.append(detailsList)
-        //details.innerHTML += `${title}`
-        //details.innerHTML += `${detailsList.innerHTML}`
-        info.append(details)
-        //info.innerHTML = `${details.innerHTML}`
-        product.className = 'product'
-        product.innerHTML += `${divImage.innerHTML}`
-        //product.innerHTML += `${info.innerHTML}`
-        product.append(info)
-        console.log('products inside', product)
-        // product.innerHTML = `<div class="image">
-        //                             <div class="ribbon left">nuevo</div>
-        //                             <img src=${productPicPath} alt="">
-        //                         </div>
-        //                             <div class="info">
-        //                             <div class="details">
-        //                                 <div class="title">
-        //                                     <br> ${productTitle}
-        //                                 </div>
+        appendProductCard(document.querySelector('.product-list'), data[i])
 
-        //                                 <ul class="caracteristicas">
-
-        //                                     <li>${productCategory} </li>
-        //                                     <li> ${productMaterial}</li>
-        //                                     <li> ${productLightType}</li>
-        //                                     <li> ${productTemperature}</li>
-
-        //                                     <li> ${productColor}</li>
-        //                                     <li> ${productHeight}</li>
-        //                                     <li> Profundidad: 86 mm</li>
-        //                                     <li> Con movimiento cardánico</li>
-
-        //                                 </ul>
-        //                             </div>
-        //                         </div>`
-        list.appendChild(product)
     }
 }
 readCatalogue()
-//let data = readCatalogue().then(data => data = data.data)
-const productList = document.querySelector('.product-list')
-const template = document.querySelector('.even')
-const productClone = template.content.cloneNode(true)
-productClone.innerHTML = `<div class="image">
-    <div class="ribbon left">PRUEBAA</div>
-    <img src="https://m.media-amazon.com/images/I/81qY64-hIlL._AC_SX679_.jpg" alt="">
-</div>
-    <div class="info">
-    <div class="details">
-        <div class="title">
-            <br> PRODUCTO TEMPLATE
-        </div>
-
-        <ul class="caracteristicas">
-
-            <li>111 </li>
-            <li> 222</li>
-            <li>3333</li>
-            <li> 4444</li>
-
-            <li> 55555</li>
-            <li> 666666</li>
-            <li> Profundidad: 86 mm</li>
-            <li> Con movimiento cardánico</li>
-
-        </ul>
-    </div>
-</div>`
-//productList.appendChild(productClone)
-const product = document.createElement('div')
-product.innerHTML = `<div class="image">
-    <div class="ribbon left">nuevo</div>
-    <img src="./Images/pruebalalala.png" alt="">
-</div>
-    <div class="info">
-    <div class="details">
-        <div class="title">
-            <br> TITULO lalalal
-        </div>
-
-        <ul class="caracteristicas">
-            <li> Aplique</li>
-            <li> Material: Aluminio y acero</li>
-            <li> Dicroica x 4</li>
-            <li> Color: Negro</li>
-
-            <li> Largo: 165 mm</li>
-            <li> Ancho: 165 mm</li>
-            <li> Profundidad: 86 mm</li>
-            <li> Con movimiento cardánico</li>
-
-        </ul>
-    </div>
-</div>`
-product.className = 'product'
-//product.innerHTML = data.data[1]// [0] 
-const list = document.querySelector('.product-list')
-
-console.log('list', list)
-//list.appendChild(product)
-//const templateEven = document.getElementsByClassName('even')
-//list.appendChild(product)
-// for (i = 1; i < data.length; i++) {
-//     if (i % 5 == 0) {
-//         currentPage++
-//         const product = document.createElement('div')
-//         //  product.className = 'product'
-//         product.innerText = data[i][0]
-//         const list = document.getElementsByClassName('product-list')
-//         list.appendChild(product)
-//     } else {
-//         const product = document.createElement('div')
-//         product.className = 'product'
-//         currentPage++
-//         //  product.className = 'product'
-//         product.innerText = data[i][0]
-//         const list = document.getElementsByClassName('product-list')
-//         list.appendChild(product)
-
-//     }
-// }
 
